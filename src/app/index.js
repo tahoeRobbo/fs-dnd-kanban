be5ae70bee00c906b7bdb9299c7b0f24b316cf8c
tahoeRobbo <robert.r.hallowell@gmail.com>
2019-12-20 14:31:19 -0600

    initial commit -- setup with react babel and webpack

---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App () {
+  return (
+    <div>Hello World!</div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'app'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/app/index.html'
+    })
+  ],
+  devServer: {
+    historyApiFallback: true
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+}
