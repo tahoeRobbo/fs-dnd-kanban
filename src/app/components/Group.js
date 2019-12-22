2e25618294fa6bafafe02b6944df16a20ffbaf8c
tahoeRobbo <robert.r.hallowell@gmail.com>
2019-12-22 15:05:05 -0600

    WIP draggable tasks and dropable groups.  todo add actions / reducers for changing group id on task when dropping into new group bucket

--- src/app/components/Group.js
+++ src/app/components/Group.js
@@ -10,8 +10,22 @@ function Group ({ group }) {
     return store.tasks.filter((task) => task.group === id)
   })
 
+  const handleDragOver = (e) => {
+    e.preventDefault()
+  }
+
+  const handleDrop = (e) => {
+    const id = e.dataTransfer.getData('id')
+    console.log('task from handleDrop', id)
+    console.log('group.id', group.id)
+  }
+
   return (
-    <div className='w-full bg-blue-300 text-center m-2'>
+    <div
+      className='w-full bg-blue-300 text-center m-2'
+      onDragOver={(e) => handleDragOver(e)}
+      onDrop={(e) => handleDrop(e)}
+    >
       <h2 className='font-bold text-2xl'>{name}</h2>
       {tasksInGroup.map((task) => (
         <Task key={task.id} task={task} />
--- src/app/components/Task.js
+++ src/app/components/Task.js
@@ -3,8 +3,17 @@ import PropTypes from 'prop-types'
 
 function Task ({ task }) {
   const { name, id } = task
+
+  const handleDragStart = (e, id) => {
+    console.log('task in handleDragStart', id)
+    e.dataTransfer.setData('id', id)
+  }
   return (
-    <div className='text-red-900 w-4/5 bg-red-100 px-10 py-3 m-auto mb-3' id={id}>
+    <div
+      draggable
+      className='text-red-900 w-4/5 bg-red-100 px-10 py-3 m-auto mb-3' id={id}
+      onDragStart={(e) => handleDragStart(e, id)}
+    >
       {name}
     </div>
   )
