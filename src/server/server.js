3aee8373444c25dea11ed9ba9b80bcf6610e6f72
tahoeRobbo <robert.r.hallowell@gmail.com>
2019-12-28 14:40:47 -0600

    express server with routes and tests for adding a new task and updating a task.  will setup mongoose at some point

---
+++ src/server/server.js
@@ -0,0 +1,68 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import { connectDB } from './connect-db'
+import { formatNewTask } from './utils/helpers'
+
+let port = 8888
+
+const app = express()
+
+app.listen(port, () => console.log(`server listening on port ${port}`))
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World')
+// })
+
+export async function addNewTask (name) {
+  let db = await connectDB()
+  let collection = db.collection('tasks')
+  let formattedTask = formatNewTask(name)
+  await collection.insertOne(formattedTask)
+  return formattedTask
+}
+
+app.use(
+  cors,
+  bodyParser.urlencoded({extended: true}),
+  bodyParser.json()
+)
+
+export async function updateTask (task) {
+  let { id, name, group, isComplete } = task
+  let collection = await connectDB().collection('tasks')
+
+  if (group) {
+    await collection.updateOne({id}, {$set:{group}})
+  }
+
+  if (name) {
+    await collection.updateOne({id}, {$set:{name}})
+  }
+
+  if (isComplete !== undefined) {
+    let completed = Date.now()
+    await collection.updateOne(
+      {id},
+      {$set: {
+          isComplete,
+          completed
+        }
+      })
+      .then(() => task.completed = completed)
+  }
+
+  return task
+}
+
+app.post('/task/new', async (req, res) => {
+  let { taskName } = req.body
+  let formattedTask = await addNewTask(taskName)
+  res.status(200).send(formattedTask)
+})
+
+app.post('/task/update', async (req, res) => {
+  let { task } = req.body
+  await updateTask(task)
+  res.status(200).send()
+})
---
+++ src/server/utils/helpers.js
@@ -0,0 +1,14 @@
+export function formatNewTask (name) {
+  return {
+    name,
+    id: generateId(),
+    group: 'G1',
+    owner: 'U1', // todo update once auth implemented,
+    created: Date.now(),
+    isComplete: false
+  }
+}
+
+function generateId () {
+  return Math.random().toString(16).substr(2, 10)
+}
